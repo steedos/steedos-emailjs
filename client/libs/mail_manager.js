feffdb25775fe440abe95f08e6f33a844a3a37c8
zhangshanshan <zhangshanshan@hotoa.com>
2016-11-13 14:51:03 +0800

    bug: 获取前前10封未读邮件的message

--- client/libs/mail_manager.js
+++ client/libs/mail_manager.js
@@ -11,8 +11,7 @@ MailManager.initMail = function(callback){
 
       ImapClientManager.initMailboxInfo(inbox, function(){
         ImapClientManager.updateUnseenMessages(function(){
-          var unseenUids = MailUnseendisplay.getUnseenUids();
-          MailUnseendisplay.listUnseenMessages(unseenUids);
+          MailUnseendisplay.listUnseenMessages();
         });
         Session.set("mailInit", true);
         Session.set("mailBoxInit", true);
