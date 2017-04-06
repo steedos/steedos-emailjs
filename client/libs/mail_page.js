21bf6d5ebe64d663f49525fd780203a643223a53
chenzhipei <chenzhipei@hotoa.com>
2017-04-06 15:51:02 +0800

    修改mail_page.js缩进为tab

--- client/libs/mail_page.js
+++ client/libs/mail_page.js
@@ -5,17 +5,17 @@ MailPage.pageSize = 20;
 
 //总页数
 MailPage.pageCount  = function(boxMessageNumber){
-    return boxMessageNumber % MailPage.pageSize === 0 ? boxMessageNumber / MailPage.pageSize : parseInt( boxMessageNumber / MailPage.pageSize) + 1;
+	return boxMessageNumber % MailPage.pageSize === 0 ? boxMessageNumber / MailPage.pageSize : parseInt( boxMessageNumber / MailPage.pageSize) + 1;
 }
 
 
 //向后翻页
- MailPage.pageBack = function(){
- 	if (Session.get("mailPage") <= 1)
- 		Session.set("mailPage", 1);
- 	else 
- 		Session.set("mailPage", Session.get("mailPage") - 1);
- }
+MailPage.pageBack = function(){
+	if (Session.get("mailPage") <= 1)
+		Session.set("mailPage", 1);
+	else 
+		Session.set("mailPage", Session.get("mailPage") - 1);
+}
 
 //向前翻页
 MailPage.pageForward = function(boxMessageNumber){
@@ -28,11 +28,11 @@ MailPage.pageForward = function(boxMessageNumber){
 
 //起始数
 MailPage.PageStart = function(boxMessageNumber){
-  var rev = (Session.get("mailPage") - 1) * MailPage.pageSize +1;
+	var rev = (Session.get("mailPage") - 1) * MailPage.pageSize +1;
 	if(rev > boxMessageNumber)
 		rev = boxMessageNumber - (boxMessageNumber % MailPage.pageSize) + 1;
-  console.log("MailPage.PageStart ......")
- 	return rev;
+	console.log("MailPage.PageStart ......")
+	return rev;
 }
 
 //结束数
